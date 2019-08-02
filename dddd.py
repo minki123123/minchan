04b211b6299916db08a09d6436f7ac911cd8c573
minki123123 <53622695+minki123123@users.noreply.github.com>
2019-08-03 02:39:09 +0900

    Add files via upload

---
+++ dddd.py
@@ -0,0 +1,38 @@
+import discord
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print(client.user.id)
+    print("ready")
+    game = discord.Game("여캠 탐방")
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("~채널메시지"):
+        channel = message.content[7:25]
+        msg = message.content[26:]
+        await client.get_channel(int(channel)).send(msg)
+
+@client.event
+async def on_member_join(member):
+    my_guild = client.get_guild(602815804588687371)
+    if (member.guild.id == my_guild.id):
+        channel = client.get_channel(606891813885837356)
+        channel_name = '{}_members'.format(my_guild.member_count)
+        await channel.edit(name=channel_name)
+
+@client.event
+async def on_member_remove(member):
+    my_guild = client.get_guild(602815804588687371)
+    if (member.guild.id == my_guild.id):
+        channel = client.get_channel(606891813885837356)
+        channel_name = '{}_members'.format(my_guild.member_count)
+        await channel.edit(name=channel_name)
+
+
+
+client.run("NjA2ODkyNTQxOTU0MjI4MjM1.XURqvQ.88a0u64pdMu5_MRa1fdt9ja488k")
